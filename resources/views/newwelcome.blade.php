052726de825025b883854297c03d85a5b0a72afb
Sami Tariq <maddawgkilla@gmail.com>
2020-03-23 08:45:13 -0400

    545 23 March

--- resources/views/newwelcome.blade.php
+++ resources/views/newwelcome.blade.php
@@ -1,21 +1,6 @@
 @extends('newlayout')
 
-@section('content')
-<div id="header-wrapper">
-	<div id="header" class="container">
-		<div id="logo">
-			<h1><a href="#">SimpleWork</a></h1>
-		</div>
-		<div id="menu">
-			<ul>
-				<li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
-				<li><a href="#" accesskey="2" title="">Our Clients</a></li>
-				<li><a href="/about" accesskey="3" title="">About Us</a></li>
-				<li><a href="#" accesskey="4" title="">Careers</a></li>
-				<li><a href="#" accesskey="5" title="">Contact Us</a></li>
-			</ul>
-		</div>
-	</div>
+@section('header-featured')
 	<div id="header-featured">
 		<div id="banner-wrapper">
 			<div id="banner" class="container">
@@ -24,5 +9,4 @@
 				<a href="#" class="button">Etiam posuere</a> </div>
 		</div>
 	</div>
-</div>
 @endsection
